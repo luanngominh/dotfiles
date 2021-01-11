9b5d7552a8db7fde479eee28932f03f76a207fd2
luanngominh <ngominhluanbox@gmail.com>
2021-01-12 01:40:34 +0700

    add update script

--- update.sh
+++ update.sh
@@ -1,2 +1,24 @@
 #!/usr/bin/env sh
-cp ~/.spacemacs .spacemacs
+
+help="Type \"$0 commit\" or \"$0 local\""
+[[ $# != 1 ]] && echo $help && exit 0
+
+# Define some dot file path
+SPACEMACS_LOCAL="~/.spacemacs"
+ZSH_LOCAL="~/.zshrc"
+
+case $1 in 
+	commit)
+		echo "Sync spacemacs dot file"
+		cp ${SPACEMACS_LOCAL} .spacemacs
+	;;
+
+	local)
+ 		cp .spacemacs ${SPACEMACS_LOCAL}
+	;;
+
+	*)
+		echo $help
+	;;
+esac
+
